2d3ad9ff5903a26070258f707228334cd765a647
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2018-01-17 16:16:00 +0000

    Install app, clean up models, setup admin

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from pathlib import Path
 
-# Register your models here.
+from django.contrib import admin
+from django.apps import apps
+
+app = apps.get_app_config(Path(__file__).parent.name)
+
+for model in app.get_models():
+    admin.site.register(model)
