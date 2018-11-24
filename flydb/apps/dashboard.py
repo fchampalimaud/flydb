8b0e4fcc98f4b00c39021c4d3cbbc3e8a7772430
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-11-24 14:05:11 +0000

    Fly db

--- flydb/apps/dashboard.py
+++ flydb/apps/dashboard.py
@@ -1,6 +1,7 @@
 from pyforms.basewidget import BaseWidget
 from pyforms.controls import ControlText
 from pyforms.controls import ControlCheckBox
+from pyforms.controls import ControlButton
 from confapp import conf
 
 class FlyDashboard(BaseWidget):
@@ -20,5 +21,12 @@ class FlyDashboard(BaseWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._code = ControlText('Code')
-        self._print = ControlCheckBox('Send to printer')
+        self._code    = ControlText('Code', on_enter_event=self.__find_btn_evt)
+        self._print   = ControlCheckBox('Send to printer', label_visible=False)
+        self._findbtn = ControlButton('Find', label_visible=False, default=self.__find_btn_evt)
+
+        self.formset  = ['_print', '_code', '_findbtn']
+
+
+    def __find_btn_evt(self):
+        pass
