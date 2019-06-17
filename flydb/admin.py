d778e87c0432433fbd9bdbc86f7621b479367e99
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-06-17 19:14:30 +0100

    Update admin

--- flydb/admin.py
+++ flydb/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Fly, Hospitalization, Specie, Location, LegacySource, Category, Source, Supplier
+from . import models
 
-admin.site.register(Fly)
-admin.site.register(Hospitalization)
-admin.site.register(Specie)
-admin.site.register(Location)
-admin.site.register(LegacySource)
-admin.site.register(Category)
-admin.site.register(Source)
-admin.site.register(Supplier)
+admin.site.register(models.Fly)
+admin.site.register(models.Specie)
+admin.site.register(models.Category)
+admin.site.register(models.Location)
+admin.site.register(models.LegacySource)
+admin.site.register(models.Source)
+admin.site.register(models.Hospitalization)
+# admin.site.register(models.Supplier)
