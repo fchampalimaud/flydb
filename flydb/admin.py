42aba5c52f6c93d54492e9dc677ee548ce0bb7ca
Luís Teixeira <micboucinha@gmail.com>
2019-12-03 16:04:36 +0000

    Add origin_internal, maintainer and ownership fields to FlyResource

--- flydb/admin.py
+++ flydb/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 from import_export import resources, widgets
 from import_export.admin import ExportActionMixin, ImportMixin
 from import_export.fields import Field
@@ -6,16 +7,18 @@ from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 
 from . import models
 from .models import Fly, Species, Category, Location, LegacySource, Source, StockCenter, Hospitalization
+from users.models import Group
 
 
 class FlyResource(resources.ModelResource):
     origin_center = Field(attribute='origin_center', column_name='origin_center', widget=ForeignKeyWidget(StockCenter, 'name'))
     species = Field(attribute='species', column_name='species', widget=ForeignKeyWidget(Species, 'name'))   
     legacysource = Field(attribute='legacysource', column_name='legacysource', widget=ForeignKeyWidget(LegacySource, 'name'))   
-    #origin_internal = Field(attribute='origin_internal', column_name='origin_internal', widget=ForeignKeyWidget(users.Group??, 'name'))
+    origin_internal = Field(attribute='origin_internal', column_name='origin_internal', widget=ForeignKeyWidget(Group, 'name'))
     categories = Field(attribute='categories', column_name='categories', widget=ManyToManyWidget(Category, field='name'))
+    maintainer = Field(attribute='maintainer', column_name='maintainer', widget=ForeignKeyWidget(settings.AUTH_USER_MODEL, 'name'))
+    ownership = Field(attribute='ownership', column_name='ownership', widget=ForeignKeyWidget(Group, 'name'))
 
-    #TODO maintainer and ownership FKs missing
     class Meta:
         model = Fly
         skip_unchanged = True
