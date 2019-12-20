f9e313ea72f3cca5006881f6fc43447d757a72a8
Luís Teixeira <micboucinha@gmail.com>
2019-12-20 19:02:03 +0000

    Update urls.py

--- flydb/urls.py
+++ flydb/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import print_barcode
 
 urlpatterns = [
-    path(r"^findstock/(?P<barcode>[\w\d\-\.]+)/$", "flydb.views.findstock"),
-    path(r"^flipnow/(?P<id>\d+)/$", "flydb.views.flipnow"),
-    path(r"^printbarcode/(?P<fly_url>\d+)/$", "flydb.views.print_fly_barcode"),
-    path(r"^flipbylocation/(?P<loc>\w+)/$", "flydb.views.flipbylocation"),
-    path(r"^admin_tools/", include("admin_tools.urls")),
-    path(r"^account/", include("django_authopenid.urls")),
-    path(r"^", include(admin.site.urls)),
+    # TODO: cleanup these paths
+    # path(r"^findstock/(?P<barcode>[\w\d\-\.]+)/$", "flydb.views.findstock"),
+    # path(r"^flipnow/(?P<id>\d+)/$", "flydb.views.flipnow"),
+    path("print_barcode/<int:fly_url>", print_barcode, name="print_barcode"),
+    # path(r"^flipbylocation/(?P<loc>\w+)/$", "flydb.views.flipbylocation"),
+    # path(r"^admin_tools/", include("admin_tools.urls")),
+    # path(r"^account/", include("django_authopenid.urls")),
+    # path(r"^", include(admin.site.urls)),
 ]
