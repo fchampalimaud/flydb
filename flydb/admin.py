fc3e1355a5aaa2ba3de01e45f58e8c045580f8d7
Luís Teixeira <micboucinha@gmail.com>
2020-03-09 14:29:39 +0000

    Define export fields order as used in the app form

--- flydb/admin.py
+++ flydb/admin.py
@@ -29,6 +29,11 @@ class FlyResource(resources.ModelResource):
         skip_unchanged = True
         clean_model_instances = True
         exclude = ('id',)
+        export_order = ('species', 'flybase_id', 'internal_id', 'location', 'categories', 'died', 'public', 
+                        'origin', 'origin_center', 'origin_id', 'origin_obs', 
+                        'chrx', 'chry', 'chr2', 'chr3', 'chr4', 'bal1', 'bal2', 'bal3', 'chru',
+                        'wolbachia', 'wolbachia_treatment_date', 'virus_treatment_date', 'isogenization_background', 'special_husbandry_conditions',
+                        'line_description', 'comments' )
 
     def before_import_row(self, row, **kwargs):
         for field, value in row.items():
